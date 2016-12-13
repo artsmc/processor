caca11b13ba2373f3c54f67cadfa2e2ef82ad447
Mark Campbell <mark.campbell@progeny.net>
2016-12-13 12:53:17 -0500

    drag n drop buckets!

--- js/libs/main.js
+++ js/libs/main.js
@@ -3,5 +3,9 @@ define(function (require) {
 	require('autogrow');
   		$(function() {//start doing cool shit here 
 			$('textarea').autogrow({onInitialize: true});
+			var thoughts = document.getElementById('section-thoughts');
+			var section = document.getElementById('section');
+			 Sortable.create(section, { /* options */ });
+			Sortable.create(sectionThoughts, { /* options */ });
     	});//END APP
 });
